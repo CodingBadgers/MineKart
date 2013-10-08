553ed6ce26f9d5b4f6f5a97bc734daa50984797c
SamOatesGames <sam@samoatesgames.com>
2013-10-08 12:20:13 +0100

    Added default code required for a Bukkit plugin.

---
+++ src/main/java/uk/thecodingbadgers/minekart/MineKart.java
@@ -0,0 +1,26 @@
+package uk.thecodingbadgers.minekart;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author TheCodingBadgers
+ *
+ * Main entry class for the MineKart plugin
+ *
+ */
+public final class MineKart extends JavaPlugin {
+	 
+	/**
+	 * Called when the plugin is enabled
+	 */
+	public void onEnable() {
+		
+	}
+ 
+	/**
+	 * Called when the plugin is disabled
+	 */
+	public void onDisable() {
+		
+	}
+}
