8891d02a49fc7541a5f803def1aa3aa60a9c08ce
James Fitzpatrick <thefishlive@hotmail.com>
2013-10-21 21:07:25 +0100

    Add basic lobby join signs Only decorative for the moment, need to add functionality

--- src/main/java/uk/thecodingbadgers/minekart/listener/BlockListener.java
+++ src/main/java/uk/thecodingbadgers/minekart/listener/BlockListener.java
@@ -3,13 +3,17 @@ package uk.thecodingbadgers.minekart.listener;
 import java.util.Map;
 
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
 
 import uk.thecodingbadgers.minekart.MineKart;
+import uk.thecodingbadgers.minekart.lobby.LobbySign;
+import uk.thecodingbadgers.minekart.lobby.LobbySignManager;
 import uk.thecodingbadgers.minekart.racecourse.Racecourse;
 
 /**
@@ -78,6 +82,37 @@ public class BlockListener implements Listener {
 			}
 		}
 		
+	}
+	
+	@EventHandler
+	public void onSignChange(SignChangeEvent event) {
+	    Player player = event.getPlayer();
+	    
+	    if (!player.hasPermission("minekart.lobby.create")) {
+	        return;
+	    }
+	    
+	    if (!event.getLine(0).equalsIgnoreCase("[MineKart]")) {
+	        return;
+	    }
+	    
+	    String coursename = event.getLine(1);
+	    Racecourse course = MineKart.getInstance().getRacecourse(coursename);
+	    
+	    if (course == null) {
+	        return;
+	    }
+	    
+	    LobbySign sign = new LobbySign(event.getBlock(), course);
+	    LobbySignManager.addSign(sign);
+	    
+	    String[] lines = sign.getSignContent();
+	    
+	    for (int i = 0; i < 4; i++) {
+	        event.setLine(i, lines[i]);
+	    }
+	    
+	    MineKart.output(player, "You have created a lobby sign for " + course.getName());
 	}
 	
 }
