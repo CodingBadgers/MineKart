4d8f2c2bc2d175b344f9487f3d1e444b3c8cf26b
James Fitzpatrick <thefishlive@hotmail.com>
2013-11-23 15:30:39 +0000

    Fix javadocs in FireworkFactory

--- src/main/java/uk/thecodingbadgers/minekart/util/FireworkFactory.java
+++ src/main/java/uk/thecodingbadgers/minekart/util/FireworkFactory.java
@@ -14,8 +14,16 @@ import org.bukkit.inventory.meta.FireworkMeta;
 
 public class FireworkFactory {
 
-	/*
+	/**
 	 * Launch a firework at a given location with specified properties
+	 * 
+	 * @param spawnLocation the location for the firework
+	 * @param type the firework type
+	 * @param power the power of the firework
+	 * @param colors the colors of the fireworks
+	 * @param fadecolors the colors for the firework to fade to
+	 * @param trail if the firework should leave a trail
+	 * @param flicker if the firework should flicker
 	 */
 	static public Firework LaunchFirework(Location spawnLocation, FireworkEffect.Type type, int power, ArrayList<Color> colors, ArrayList<Color> fadecolors, boolean flicker, boolean trail) {
 
@@ -38,9 +46,14 @@ public class FireworkFactory {
 		return firework;
 	}
 
-	/*
+	/**
 	 * Launch a firework at a given location with specified properties Will
 	 * use same fade color as color and have trail and flicker enabled
+	 * 
+	 * @param spawnLocation the location for the firework
+	 * @param type the firework type
+	 * @param power the power of the firework
+	 * @param colors the colors of the fireworks
 	 */
 	static public Firework LaunchFirework(Location spawnLocation, FireworkEffect.Type type, int power, Color color) {
 		ArrayList<Color> colors = new ArrayList<Color>();
@@ -49,6 +62,13 @@ public class FireworkFactory {
 		return LaunchFirework(spawnLocation, type, power, colors, colors, true, true);
 	}
 	
+	/**
+	 * Display a firework explosion at a location
+	 * 
+	 * @param spawnLocation the location for the explosion
+	 * @param type the firework effect type
+	 * @param color the color of the fireworks
+	 */
 	static public void SpawnFireworkExplosion(Location spawnLocation, FireworkEffect.Type type, Color color) {
 		try {
 			Firework firework = LaunchFirework(spawnLocation, type, 1, color);
