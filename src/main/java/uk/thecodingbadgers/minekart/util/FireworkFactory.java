1a56d45c17cc4342c042edd124ea846438eea57d
James Fitzpatrick <thefishlive@hotmail.com>
2014-01-18 17:00:19 +0000

    Updated firework factory to use new bukkit detonate method

--- src/main/java/uk/thecodingbadgers/minekart/util/FireworkFactory.java
+++ src/main/java/uk/thecodingbadgers/minekart/util/FireworkFactory.java
@@ -6,9 +6,6 @@ import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Builder;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_7_R2.CraftWorld;
-import org.bukkit.craftbukkit.v1_7_R2.entity.CraftFirework;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Firework;
 import org.bukkit.inventory.meta.FireworkMeta;
 
@@ -27,7 +24,7 @@ public class FireworkFactory {
 	 */
 	static public Firework LaunchFirework(Location spawnLocation, FireworkEffect.Type type, int power, ArrayList<Color> colors, ArrayList<Color> fadecolors, boolean flicker, boolean trail) {
 
-		Firework firework = (Firework) spawnLocation.getWorld().spawnEntity(spawnLocation, EntityType.FIREWORK);
+		Firework firework = spawnLocation.getWorld().spawn(spawnLocation, Firework.class);
 		FireworkMeta metadata = firework.getFireworkMeta();
 
 		Builder builder = FireworkEffect.builder();
@@ -70,14 +67,8 @@ public class FireworkFactory {
 	 * @param color the color of the fireworks
 	 */
 	static public void SpawnFireworkExplosion(Location spawnLocation, FireworkEffect.Type type, Color color) {
-		try {
-			Firework firework = LaunchFirework(spawnLocation, type, 1, color);
-			CraftWorld world = (CraftWorld) spawnLocation.getWorld();
-			world.getHandle().broadcastEntityEffect(((CraftFirework)firework).getHandle(), (byte) 17);
-			firework.remove();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+		Firework firework = LaunchFirework(spawnLocation, type, 1, color);
+		firework.detonate();
 	}
 
 }
